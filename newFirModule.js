bd3c327ba380e1112ade525ecc4b5ea19ce6413c
valemisem <valentina.misem@gmail.com>
2023-11-15 16:50:17 +0100

    commit

--- newFirModule.js
+++ newFirModule.js
@@ -1,6 +1,14 @@
 import logName from "./function.js" // it was EXPORT DEFAULT
 import {textText} from "./function.js" // it was EXPORT
 
+import chai from 'chai'
+
+
 logName('M', ' valya')  
 textText('6', ' 5')  
 
+chai.assert.include('my string', 'hjhj', 'no') // class assert, method include
+ // это мы рассмотрели библиотеку chai.
+ // ещё мы должны подключить jest.
+
+ 
